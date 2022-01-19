33fe6b4cdea4b7e5c4753b172b364427d6e1faf1
RaquelAlvarezz <raquel.alvarez@vigo.org>
2022-01-19 09:03:08 +0100

    changues

---
+++ obtener-hash.php
@@ -0,0 +1,14 @@
+<?php
+echo password_hash("HarryPotter",  PASSWORD_BCRYPT, ['cost'=>12]) ?>
+?>
+<?php
+echo password_hash("hogwarts",  PASSWORD_BCRYPT, ['cost'=>12]) ?>
+?>
+<?php
+echo password_hash("kimberly",  PASSWORD_BCRYPT, ['cost'=>12]) ?>
+?>
+
+
+
+
+
